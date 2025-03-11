ddeedec1cce0d30351a430018ce01f01f5ef7e7c
Kaiden Keane <kjkeane88@gmail.com>
2025-03-11 12:07:38 -0600

    remove mapping <C-e> to Ex because of nvim-tree

--- lua/kaiden/core/mappings.lua
+++ lua/kaiden/core/mappings.lua
@@ -1,3 +1,2 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-vim.keymap.set("t", "<C-e>", [[<C-\><C-n>]], {noremap = true} )
+vim.keymap.set("t", "<C-e>", [[<C-\><C-n>]], {noremap = true} ) -- get out of terminal by control + e
