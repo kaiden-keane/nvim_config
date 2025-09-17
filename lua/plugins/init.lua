6b138a9129faed16c1cb6ecccb73123616ee24b8
kaiden <kkeane@ualberta.ca>
2025-09-16 19:07:28 -0600

    lets see if this works

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,19 @@
 return {
     -- simple plugins that dont need much
-    { "folke/tokyonight.nvim", config = function() vim.cmd.colorscheme "tokyonight" end }
-    -- tmux for example
+    {
+        "catppuccin/nvim", 
+        config = function ()
+            catppuccin = require("catppuccin")
+            catppuccin.setup()
+            catppuccin.setup({
+                flavour = "auto", -- latte, frappe, macchiato, mocha
+                background = { -- :h background
+                light = "latte",
+                dark = "mocha",
+                }    
+            })
+            vim.cmd.colorscheme "catppuccin"
+        end,
+        endname = "catppuccin", priority = 1000 }
+    -- { "folke/tokyonight.nvim", config = function() vim.cmd.colorscheme "tokyonight" end }
 }
