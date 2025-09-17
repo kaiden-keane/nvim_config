12f52170e4f6ecc3e7f0b159c3e013cdaa6a7531
Kaiden Keane <kjkeane88@gmail.com>
2025-09-16 23:06:11 -0600

    change colourscheme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,19 +1,32 @@
 return {
     -- simple plugins that dont need much
     {
-        "catppuccin/nvim", 
-        config = function ()
-            catppuccin = require("catppuccin")
-            catppuccin.setup()
-            catppuccin.setup({
-                flavour = "auto", -- latte, frappe, macchiato, mocha
-                background = { -- :h background
-                light = "latte",
-                dark = "mocha",
-                }    
-            })
-            vim.cmd.colorscheme "catppuccin"
-        end,
-        endname = "catppuccin", priority = 1000 }
+      "neanias/everforest-nvim",
+      version = false,
+      lazy = false,
+      priority = 1000, -- make sure to load this before all the other start plugins
+      -- Optional; default configuration will be used if setup isn't called.
+      config = function()
+        require("everforest").setup({
+          -- Your config here
+        })
+        vim.cmd.colorscheme "everforest"
+      end,
+    }
+    -- {
+    --     "catppuccin/nvim", 
+    --     config = function ()
+    --         catppuccin = require("catppuccin")
+    --         catppuccin.setup()
+    --         catppuccin.setup({
+    --             flavour = "auto", -- latte, frappe, macchiato, mocha
+    --             background = { -- :h background
+    --             light = "macchiato",
+    --             dark = "mocha",
+    --             }    
+    --         })
+    --         vim.cmd.colorscheme "catppuccin"
+    --     end,
+    --     endname = "catppuccin", priority = 1000 }
     -- { "folke/tokyonight.nvim", config = function() vim.cmd.colorscheme "tokyonight" end }
 }
